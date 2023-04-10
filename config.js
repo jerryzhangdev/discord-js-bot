fcd9d897b10975850d6fbdf6cb931a8bf666bfd2
hydrogen-studio <egret.support@virtualegret.com>
2023-04-10 12:13:17 -0700

    test

--- config.js
+++ config.js
@@ -37,9 +37,9 @@ module.exports = {
 
   DASHBOARD: {
     enabled: true, // enable or disable dashboard
-    baseURL: "http://localhost:8080", // base url
-    failureURL: "http://localhost:8080", // failure redirect url
-    port: "8080", // port to run the bot on
+    baseURL: "https://dashboard.swiftcraft.net", // base url
+    failureURL: "https://dashboard.swiftcraft.net", // failure redirect url
+    port: "80", // port to run the bot on
   },
 
   ECONOMY: {
