0841324bd2f2e57a2b6d15c1384b782eaba32c97
hydrogen-studio <egret.support@virtualegret.com>
2023-04-11 14:15:24 -0700

    test

--- src/handlers/events.js
+++ src/handlers/events.js
@@ -2,12 +2,12 @@ const { getSettings } = require("@schemas/Guild");
 
 
 async function handleEvent(client) {
-  let guilds = client.guilds.cache.map(g => g.id);
+  let guilds = client.guilds.cache.map(g => g);
   console.log(guilds)
-  // for (var i = 0; i < guilds.length; i++) {
-  //   const settings = await getSettings(guilds[i]);
-  //   console.log(settings)
-  // }
+  for (var i = 0; i < guilds.length; i++) {
+    const settings = await getSettings(guilds[i]);
+    console.log(settings)
+  }
 }
 
 module.exports = {
