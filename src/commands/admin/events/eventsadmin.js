898c8a785a51a44e5c38daea6500305de795a25d
hydrogen-studio <egret.support@virtualegret.com>
2023-04-11 17:47:25 -0700

    test

--- src/commands/admin/events/eventsadmin.js
+++ src/commands/admin/events/eventsadmin.js
@@ -214,7 +214,7 @@ async function contactParticipants(interaction, settings) {
   for(var k = 0; k < participants.length; k++) {
     try{
       let dm = new EmbedBuilder()
-      .setTitle("Event Contact")
+      .setTitle("Event Organizer for the event " + interaction.options.getString("name") + " in " + interaction.guild.name + " has sent you a message!")
       .setDescription(interaction.options.getString("message"))
       .setColor(EMBED_COLORS.BOT_EMBED)
       participants[k].send({ embeds: [dm] })
