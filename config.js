08595b2dca8083eabef358eca703901a76cd27b7
hydrogen-studio <egret.support@virtualegret.com>
2023-04-11 18:13:31 -0700

    test

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   INTERACTIONS: {
     SLASH: true, // Should the interactions be enabled
     CONTEXT: true, // Should contexts be enabled
-    GLOBAL: false, // Should the interactions be registered globally
+    GLOBAL: true, // Should the interactions be registered globally
     TEST_GUILD_ID: "1095515374449070100", // Guild ID where the interactions should be registered. [** Test you commands here first **]
   },
   EMBED_COLORS: {
